907cd75e3daf0ce0a442b9961656645087e1512c
dyegonog <dyegoec@outlook.com>
2017-02-13 00:16:18 -0300

    Iniciando a home

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -7,18 +7,21 @@ import { RouterModule, Routes } from '@angular/router'
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { HomeComponent } from './home/home.component';
 
 const router: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'cadastro', component: CadastroComponent }
+  { path: 'cadastro', component: CadastroComponent }, 
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    CadastroComponent
+    CadastroComponent, 
+    HomeComponent
   ],
   imports: [
     BrowserModule,

afb30a3b2f201f7e60e9ce3f5d1b74b3b525615a
dyegonog <dyegoec@outlook.com>
2017-02-15 19:57:22 -0300

    removendo mais arquivos desncessarios

--- webapp/src/assets/js/firebase.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  apiKey: "AIzaSyDktrJPuUoNPSAi703DHUjS-dp9qIOCs4o",
-  authDomain: "first-firebase-4d33e.firebaseapp.com",
-  databaseURL: "https://first-firebase-4d33e.firebaseio.com",
-  storageBucket: "first-firebase-4d33e.appspot.com",
-  messagingSenderId: "284069989885"
-};
-firebase.initializeApp(config);
